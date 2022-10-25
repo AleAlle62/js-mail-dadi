0a5d092b52a30fbdeeab110be858ea4d5c04a25d
AleAlle62 <basketallegrini@gmail.com>
2022-10-25 18:36:28 +0200

    fine, non torna con il bottone

--- JS/script.js
+++ JS/script.js
@@ -18,13 +18,12 @@ PROCEDIMENTO ES 1
 //soluzione con INCLUDES
 //BONUS
 
-let inputEmail = document.querySelector('.htmlmail')
+let inputEmail = document.getElementById('.mail')
 let btnConferma = document.querySelector('.conferma')
-let Risultato = document.querySelector('.risultatomail')
-// let Risultato2 = document.querySelector('.risultatomail2')
+let risultato1 = document.querySelector('.risultatomail1')
+let risultato2 = document.querySelector('.risultatomail2')
 
-let inputArray = ['basketallegrini@gmail.com', 'alessio.allegrinia@gmail.com', 'a.allegrini2@gmail.com']
-
+let inputArray = ['1','basketallegrini@gmail.com', 'alessio.allegrinia@gmail.com', 'a.allegrini2@gmail.com']
 
 
 
@@ -33,36 +32,53 @@ btnConferma.addEventListener('click', function(){
     
     if (inputArray.includes(inputEmail)){
         console.log('email presente')
-        Risultato.innerHTML = 'CORRETTO <i class="fa-solid fa-square-check"></i>'
-
+        risultato1.innerHTML = 'CORRETTO'
     } else {
         console.log('email NON presente')
-        Risultato.innerHTML = 'SBAGLIATO <i class="fa-solid fa-triangle-exclamation"></i>'
-    }
-
-
-    
+        risultato2.innerHTML = 'SBAGLIATO'
+    }   
 })
 
+// non funziona il CORRETTO, nel precedente push, quello senza gli innher e senza la function funziona tutto, come mai? 
+
+//SOLUZIONE CHE FUNZIONA SENZA HINNER
+
+
+
+let inputArray2 = ['basketallegrini@gmail.com', 'alessio.allegrinia@gmail.com', 'a.allegrini2@gmail.com']
+let inputEmail2 = prompt('inserisci la tua mail:')
+
+
+if (inputArray2.includes(inputEmail2)){
+    console.log('email presente')
+} else {
+    console.log('email NON presente')
+}
 
 
 
 
-//soluzione con il FOR (che non restituisce nulla se la mail non ne fa parte)
 
-// let inputArray2 = ['basketallegrini@gmail.com', 'alessio.allegrinia@gmail.com', 'a.allegrini2@gmail.com']
-// let inputEmail2 = prompt('inserisci la tua mail:')
 
-// for ( let i = 0; i < inputArray2.length ; i++) {
 
-//     if (inputEmail2 === inputArray2[i]){
-//         console.log ('email presente')
-//     } 
 
-//     // else {
-//     //     console.log('email NON presente')
-//     // }
-// }
+/*
+soluzione con il FOR (che non restituisce nulla se la mail non ne fa parte)
+
+let inputArray2 = ['basketallegrini@gmail.com', 'alessio.allegrinia@gmail.com', 'a.allegrini2@gmail.com']
+let inputEmail2 = prompt('inserisci la tua mail:')
+
+for ( let i = 0; i < inputArray2.length ; i++) {
+
+    if (inputEmail2 === inputArray2[i]){
+        console.log ('email presente')
+    } 
+
+    else {
+        console.log('email NON presente')
+    }
+}
+*/
 
 
 /*
@@ -82,26 +98,28 @@ PROCEDIMENTO ES 2
 
 */
 
-// function computer(max) {
-//     return Math.floor(Math.random(1) * max);
-// }
-// function player(max) {
-//     return Math.floor(Math.random(2) * max);
-// }
-
-// // computer
-// console.log('il PC ha fatto:' + computer(7));
-
-// // player 
-// console.log('il PLAYER ha fatto:' + player(7));
 
 
-// if ( Math.random(2) === Math.random(1)){
-//     console.log('pareggio')
-// } else if ( Math.random(2) > Math.random(1)){
-//     console.log('quindi ha vinto il PLAYER')
-// } else {
-//     console.log('quindi ha vinto il PC')
-// }
+function computer(max) {
+    return Math.floor(Math.random(1) * max);
+}
+function player(max) {
+    return Math.floor(Math.random(2) * max);
+}
+
+// computer
+console.log('il PC ha fatto:' + computer(7));
+
+// player 
+console.log('il PLAYER ha fatto:' + player(7));
+
+
+if ( Math.random(2) === Math.random(1)){
+    console.log('pareggio')
+} else if ( Math.random(2) > Math.random(1)){
+    console.log('quindi ha vinto il PLAYER')
+} else {
+    console.log('quindi ha vinto il PC')
+}
 
 
